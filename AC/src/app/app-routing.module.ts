8a9d5710d53ddf796b603b4bdbc24cd9887430a1
DharaniDharanA <dharanidharan.a2019ece@gmail.com>
2023-05-06 13:43:44 +0530

    3rd commit

--- AC/src/app/app-routing.module.ts
+++ AC/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
